fc156aada9af790e20764625d6e18f5d41eda254
arthwrvl <joaobob63@gmail.com>
2023-04-13 09:15:00 -0300

    request groups, subgroups and services for server

--- src/pages/servicebook/adminservicebook/index.tsx
+++ src/pages/servicebook/adminservicebook/index.tsx
@@ -1,9 +1,12 @@
 import Head  from 'next/head';
 import React, { useEffect, useState } from 'react'
 import AdminServiceBookTypeList from './AdminServicebookTypeList';
-import { getAllGroups } from '../../../Utils/server/getInfo';
+import { getAllGroups, getAllServices } from '../../../Utils/server/getInfo';
 import { getAllSubGroups } from '../../../Utils/server/getInfo';
 import { useRouter } from 'next/router';
+import { group } from 'console';
+import { Group, Service, SubGroup } from '../../../Utils/server/types';
+import List from '../../../pages/list';
 
 interface AdminProps {
   list: any[];
@@ -12,15 +15,38 @@ interface AdminProps {
 
 export default function AdminServiceBook(props: AdminProps) {
 // TODO fazer a requisição de todos os grupos, subgrupos e serviços
-  const [list, setList] = useState<[]>([])
+  const [groupList, setGroupList] = useState<Group[]>([])
+  const [subgroupList, setSubgroupList] = useState<SubGroup[]>([])
+  const [serviceList, setServiceList] = useState<Service[]>([])
   const router = useRouter()
 
   useEffect(() => {
    
     if (!router.isReady) return;
     const fetchData = async () => {
+      // * pegando todos os grupos
       const groups = await getAllGroups()
-      setList(groups)
+      setGroupList(groups)
+      // adicionando todos as promises em uma lista 
+      let subgroupsPromise: Promise<SubGroup>[] = [];
+      groups.forEach((group: Group) => {
+        subgroupsPromise = [...subgroupsPromise, getAllSubGroups(group.id)]
+      });
+
+      // * pegando todos os subgrupos de cada grupo
+      const subgroups = await Promise.all(subgroupsPromise);
+      setSubgroupList(subgroups);
+
+      // adicionando todos as promises em uma lista 
+
+      let servicesPromise: Promise<Service>[] = [];
+      subgroups.forEach((subgroup: SubGroup) => {
+        servicesPromise = [...servicesPromise, getAllServices(subgroup.id)]
+      });
+
+      // * pegando todos os serviços de cada subgrupo
+      const services = await Promise.all(servicesPromise);
+      setServiceList(services);
     }
     fetchData()
   }, [router.isReady])
@@ -32,7 +58,7 @@ export default function AdminServiceBook(props: AdminProps) {
         <title>Administrar servicebook</title>
       </Head>
       <div className='h-full p-8'> 
-        <AdminServiceBookTypeList list={list} title={"Grupos"} />     
+        <AdminServiceBookTypeList list={groupList} title={"Grupos"} />     
       </div>
     </>
   )
